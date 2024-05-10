e93f6e584084f54fdcc0baca32ae6bf2935e8a04
nate-smalls <nate@transposit.com>
2024-05-10 08:01:19 -0700

    update for autoscaling of eks assets

--- build_template.lua
+++ build_template.lua
@@ -136,3 +136,43 @@ module "lb_role" {
       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
       }
   }
+}
+resource "helm_release" "alb-controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  depends_on = [
+      kubernetes_service_account.service-account
+  ]
+ 
+  set {
+      name  = "region"
+      value = var.main-region
+  }
+ 
+  set {
+      name  = "vpcId"
+      value = var.vpc_id
+  }
+ 
+  set {
+      name  = "image.repository"
+      value = "602401143452.dkr.ecr.${var.main-region}.amazonaws.com/amazon/aws-load-balancer-controller"
+  }
+ 
+  set {
+      name  = "serviceAccount.create"
+      value = "false"
+  }
+ 
+  set {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+  }
+ 
+  set {
+      name  = "clusterName"
+      value = var.cluster_name
+  }
+  }
